792cbf3adaea7056c5d9cefd6821993e34511414
Evelyn Daniela Sanchéz Correa <edasancomev@gmail.com>
2020-11-19 01:40:47 +0000

    lista

--- front-end-PsicoColor/src/paginas/GradoOctavo.jsx
+++ front-end-PsicoColor/src/paginas/GradoOctavo.jsx
@@ -1,31 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/cuenta.css";
+import "bootstrap/dist/css/bootstrap.css";
 
-class Cuenta extends React.Component {
+class GradoOctavo extends React.Component {
   render() {
     return (
       <div>
         <div class="card">
           <nav>
-            <ul>
-              <li>
-                {" "}
-                <Link to="/home">Grupos</Link>
-                <ul class="children">
-                  <li>
-                    {" "}
-                    <Link to="/quiz">Octavo</Link>
-                  </li>
-                  <li>
-                    {" "}
-                    <Link to="/home">Noveno</Link>
-                  </li>
-                  <li>
-                    {" "}
-                    <Link to="/home">Cerar Sesion</Link>
-                  </li>
-                </ul>
+            <ul class="list-group">
+              <li class="list-group-item">Lista Estudiantes Octavo</li>
+
+              <li class="list-group-item list-group-item-primary">
+                ATEHORTUA ATEHORTUA ANDREA
+              </li>
+              <li class="list-group-item list-group-item-secondary">
+                ARCILA DUQUE MELISA
+              </li>
+              <li class="list-group-item list-group-item-success">
+                BURITICA MARIN SOFIA
+              </li>
+              <li class="list-group-item list-group-item-danger">
+                GARCIA TOVAR KARLA
+              </li>
+              <li class="list-group-item list-group-item-warning">
+                GONZALEZ MIGUEL ANGEL
+              </li>
+              <li class="list-group-item list-group-item-info">
+                LOPEZ CARVAJAL MANUELA
+              </li>
+              <li class="list-group-item list-group-item-light">
+                PALACIO NICOLAS
+              </li>
+              <li class="list-group-item list-group-item-dark">
+                OSORIO RESTREPO MARIANA
               </li>
             </ul>
           </nav>
@@ -35,4 +44,4 @@ class Cuenta extends React.Component {
   }
 }
 
-export default Cuenta;
+export default GradoOctavo;
